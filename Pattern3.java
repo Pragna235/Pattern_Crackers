2ceb7a8bf9bfb440e163328f0ef129c835e5b034
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-21 22:50:36 +0530

    Add files via upload
    Enter the value of n = 
    5
    * * * * * 
    * * * * 
    * * * 
    * * 
    *

---
+++ Pattern3.java
@@ -0,0 +1,20 @@
+package patternSums;
+
+public class Patterns {
+	//pattern3
+	public static void pattern3(int n)
+	{
+		for (int row = 0; row<n; row++)
+		{
+			for (int col = n-row; col>=1; col-- )
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+
+
+}
+

1c68f09b12f09a239028925602408f83e15aab60
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-21 22:49:10 +0530

    Add files via upload
    Class containing the main function to implement the different pattern methods of the Patterns Class in java.

---
+++ PatternDemo.java
@@ -0,0 +1,15 @@
+package patternSums;
+import java.util.Scanner;
+public class PatternDemo {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the value of n = ");
+		int n = input.nextInt();
+
+
+		Patterns1.pattern12(n);
+
+	}
+
+}

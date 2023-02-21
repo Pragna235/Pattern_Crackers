88074f52c11ef0ada52fcb720ae3632b98d42799
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-21 22:58:32 +0530

    Add files via upload
    Enter the value of n = 
    5
        *
       ***
      *****
     *******
    *********

---
+++ Pattern8.java
@@ -0,0 +1,25 @@
+package patternSums;
+public class Patterns {
+	//pattern8
+	public static void pattern8(int n)
+	{
+		for(int row = 1;row<=n;row++)
+		{
+			for(int sp = (n-row);sp>=1;sp--)
+			{
+				System.out.print(" ");
+			}
+			for(int col = 1;col<(2*row);col++)
+			{
+
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+
+
+}
+
+

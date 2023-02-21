0bdd4f008459e246a3fd8fcf29d40de60ae350f1
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-21 23:01:26 +0530

    Add files via upload
    Enter the value of n = 
    5
    * * * * * 
     * * * * 
      * * * 
       * * 
        *

---
+++ Pattern11.java
@@ -0,0 +1,26 @@
+package patternSums;
+public class Patterns {
+
+	//pattern11
+	public static void pattern11(int n)
+	{
+		for(int row = 0;row<n;row++)
+		{
+			for(int sp = 0; sp<row;sp++)
+			{
+				System.out.print(" ");
+			}
+			for(int col = 1;col<=(n-row);col++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+
+		}
+	}
+
+
+
+}
+
+

6ee66d94fc389b9c741d214819afe7505d2d6c1b
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-23 22:26:00 +0530

    Add files via upload
    23rd February 2023

---
+++ Pattern17.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+
+    for(int row =1;row<=n;row++)
+    {
+        for(int sp=1;sp<=n-row;sp++)
+        {
+            printf(" ");
+        }
+
+        for(int col1=row;col1>=1;col1--)
+        {
+            printf("%d", col1);
+        }
+
+        for(int col2=2;col2<=row;col2++)
+        {
+            printf("%d", col2);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
